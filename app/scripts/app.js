e794858a136866829e0a335a33fc12b5cb84cb02
Miles Heise <milesheise@gmail.com>
2017-09-14 11:17:15 -0400

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,23 +18,3 @@
          .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase', 'ngCookies'])
          .config(config);
  })();
-
- (function() {
-  function BlocChatCookies($cookies, $uibModal) {
-    var currentUser = $cookies.get('blocChatCurrentUser');
-    if (!currentUser || currentUser === '') {
-      $uibModal.open({
-          animation: this.animationsEnabled,
-          templateUrl: '/templates/userModal.html',
-          controller: 'UserInstanceCtrl as UserInstance'
-      }).result.then(function (results) {
-        $cookies.put('blocChatCurrentUser', results);
-        console.log(results);
-      })
-    }
-  }
-
-  angular
-    .module('blocChat')
-    .run(['$cookies', '$uibModal', BlocChatCookies]);
-})();
