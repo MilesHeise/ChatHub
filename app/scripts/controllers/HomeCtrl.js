9c47940785f1a20c4c8a0f50eca6709b72e4821a
Miles Heise <milesheise@gmail.com>
2018-01-07 10:23:08 -0500

    refactors and bug fixes

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,46 +1,60 @@
 (function() {
-    function HomeCtrl(Room, Message, $cookies) {
-      this.roomList = Room.all;
-      this.currentRoomId = '';
-      this.userName = $cookies.get('blocChatCurrentUser');
-      this.currentRoomName = "Welcome " + this.userName;
-      var self = this;
+  function HomeCtrl(Room, Message, $cookies, $window) {
+    this.roomList = Room.all;
+    this.currentRoomId = '';
+    this.userName = $cookies.get('blocChatCurrentUser');
+    this.currentRoomName = "Welcome " + this.userName;
+    var self = this;
 
-      this.setRoom = function ({$id, room}) {
-        self.currentRoomId = $id;
-        self.currentRoomName = room;
-        self.messageList = Message.getByRoomId($id);
-        this.model = '';
-        $('input').focus();
-      }
-
-      this.create = function (model) {
-        var d = new Date();
-        var message = {
-          content: model,
-          roomID: self.currentRoomId,
-          username: self.userName,
-          sentAt: d.toLocaleString(),
-        };
-
-        Message.send(message);
-        this.model = '';
-      }
-
-      this.deleteMessage = Message.delete;
-      this.deleteRoom = function(room) {
-        Room.delete(room);
-        Message.roomDelete(room);
-        if (currentRoomId == room) {
-          self.currentRoomName = "Welcome " + this.userName;
-          self.currentRoomId = '';
-        }
-      }
-
-
+    this.logOut = function() {
+      $cookies.put('blocChatCurrentUser', '');
+      $window.location.reload();
     }
 
-    angular
-        .module('blocChat')
-        .controller('HomeCtrl', ['Room', 'Message', '$cookies', HomeCtrl]);
+    this.setRoom = function({
+      $id,
+      room
+    }) {
+      self.currentRoomId = $id;
+      self.currentRoomName = room;
+      self.messageList = Message.getByRoomId($id);
+      this.model = '';
+      $('input').focus();
+    }
+
+    this.create = function(model) {
+      var d = new Date();
+      var message = {
+        content: model,
+        roomID: self.currentRoomId,
+        username: self.userName,
+        sentAt: d.toLocaleString(),
+      };
+
+      Message.send(message);
+      this.model = '';
+    }
+    // improvement: when there are a lot of messages it should scroll to show new message
+    // rather than you having to scroll down to see what you just posted. perhaps add
+    // a jquery scroll animate, or a focus command for last child?
+
+    this.deleteMessage = Message.delete;
+
+    this.deleteRoom = function(room) {
+      if (confirm("Are you sure you want to delete this room?\nIt cannot be undone.") == true) {
+        if (self.currentRoomId == room) {
+          self.currentRoomName = "Welcome " + this.userName;
+          self.currentRoomId = '';
+          self.messageList = null;
+        }
+        Room.delete(room);
+      }
+    }
+    // improvement: delete room should call a function on message controller to delete that
+    // rooms messages as well and keep database clean
+  }
+
+  angular
+    .module('blocChat')
+    .controller('HomeCtrl', ['Room', 'Message', '$cookies', '$window', HomeCtrl]);
 })();
--- app/scripts/services/Cookies.js
+++ app/scripts/services/Cookies.js
@@ -1,22 +1,25 @@
 (function() {
- function BlocChatCookies($cookies, $uibModal) {
-   var currentUser = $cookies.get('blocChatCurrentUser');
-   if (!currentUser || currentUser === '') {
-     $uibModal.open({
-         animation: this.animationsEnabled,
-         templateUrl: '/templates/userModal.html',
-         controller: 'UserCtrl as UserInstance',
-         backdrop: 'static',
-         keyboard: false
-     }).result.then(function (results) {
-       $cookies.put('blocChatCurrentUser', results);
-       console.log(results);
-     })
-   }
-   console.log(currentUser);
- }
+  function BlocChatCookies($cookies, $uibModal) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+      $uibModal.open({
+          animation: this.animationsEnabled,
+          templateUrl: '/templates/userModal.html',
+          controller: 'UserCtrl as UserInstance',
+          backdrop: 'static',
+          keyboard: false
+        }).result
+        .then(function(results) {
+          $cookies.put('blocChatCurrentUser', results);
+        })
+        .catch(function(reason) {
+          console.log("Promise was reject because " + reason);
+        })
+      currentUser = $cookies.get('blocChatCurrentUser');
+    }
+  }
 
- angular
-   .module('blocChat')
-   .run(['$cookies', '$uibModal', BlocChatCookies]);
+  angular
+    .module('blocChat')
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
 })();
